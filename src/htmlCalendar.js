7f6342191945c7e9c3e01ce5a025dd3bc5030885
tarodspj <tarod_spj@hotmail.com>
2018-01-30 07:39:55 -0500

    partiendo el draw de calendario html partido en partes

--- src/htmlCalendar.js
+++ src/htmlCalendar.js
@@ -1,27 +1,40 @@
-function listOfSessionsPerDay (sessions) {
+let listOfSessionsPerDay = function (sessions) {
   let sessionsPerThisDay = '',
       baseGoogleMapsButon = 'https://www.google.com/maps/?q=';
+      return {
+        getList : function() {
 
-  sessions.forEach( function ( session ) {
-    const name = session.name,
-        local_time = session.local_time,
-        link = session.link;
-    let location = 'to be defined',
-        mapLink = 'to be defined';
+          sessions.forEach( function ( session ) {
+            const name = session.name,
+                local_time = session.local_time,
+                link = session.link;
+            let location = 'to be defined',
+                mapLink = 'to be defined';
 
-        sessionsPerThisDay = sessionsPerThisDay + `name session: ${name}<br />`;
-        sessionsPerThisDay = sessionsPerThisDay + `time: ${local_time}<br />`;
-        sessionsPerThisDay = sessionsPerThisDay + `link meetup: <a href="${link}" title="event in meetup">enrole in meetup</a><br />`;
-    if( session.venue ) {
-        location = session.venue.name,
-        mapLink = `<a href="${baseGoogleMapsButon}${session.venue.lat},${session.venue.lon}" title="${location}">${location}</a>`;
-        //console.log(session.name + ' ' + session.local_time + ' ' + session.venue.name + ' ' + session.link + ' ' + mapLink);
-    }
-    sessionsPerThisDay = sessionsPerThisDay + `location: ${location} `;
-    sessionsPerThisDay = sessionsPerThisDay + `gmap link: ${mapLink}<br />`;
-  });
-  return sessionsPerThisDay;
-}
+                sessionsPerThisDay = sessionsPerThisDay + `name session: ${name}<br />`;
+                sessionsPerThisDay = sessionsPerThisDay + `time: ${local_time}<br />`;
+                sessionsPerThisDay = sessionsPerThisDay + `link meetup: <a href="${link}" title="event in meetup">enrole in meetup</a><br />`;
+            if( session.venue ) {
+                location = session.venue.name,
+                mapLink = `<a href="${baseGoogleMapsButon}${session.venue.lat},${session.venue.lon}" title="${location}">${location}</a>`;
+                //console.log(session.name + ' ' + session.local_time + ' ' + session.venue.name + ' ' + session.link + ' ' + mapLink);
+            }
+            sessionsPerThisDay = sessionsPerThisDay + `location: ${location} `;
+            sessionsPerThisDay = sessionsPerThisDay + `gmap link: ${mapLink}<br />`;
+          });
+          return sessionsPerThisDay;
+        },
+        getClass : function() {
+          let classSession = 'noSession';
+
+          if( sessions.length !== 0 ) {
+            classSession = 'yesSession';
+          }
+          return classSession;
+        }
+      }
+
+};
 
 
 export function htmlCalendar( days ) {
@@ -31,25 +44,25 @@ export function htmlCalendar( days ) {
         startWeekSessionsHtml = '<ol class="calendarHomeSessionsLine">',
         endWeekSessionsHtml = '</ol>';
 
-
   let html = startWeekDaysHtml,
       weeklySessions = startWeekSessionsHtml,
       i = 0;
 
   for (let [key, value] of days.entries()) {
-    let classSession = 'noSession';
+    let sessionsListDay = listOfSessionsPerDay(value.sessions);
 
     weeklySessions = weeklySessions + `<li id="sessionsOfDay${key}" class="session">`;
     if( value.sessions.length !== 0 ) {
-      classSession = 'yesSession';
 
-      weeklySessions = weeklySessions + listOfSessionsPerDay(value.sessions)
+      // sessionsListDay = listOfSessionsPerDay(value.sessions);
+      //console.log(theList);
+      weeklySessions = weeklySessions + sessionsListDay.getList();
     } else {
       weeklySessions = weeklySessions + `<p>today we don't have session on the schedule</p>`;
     }
     weeklySessions = weeklySessions + `</li>`;
 
-    html = html + `<li class="dayCalendar ${value.classInCalendar} ${classSession}" data-day_index="${key}">
+    html = html + `<li class="dayCalendar ${value.classInCalendar} ${sessionsListDay.getClass()}" data-day_index="${key}">
                     ${value.dayNumber}
                   </li>`;
 
